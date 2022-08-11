2dfdebd6a251c06876c8c0d5763a686d9c54fd9a
ktny <katsunori.t10@gmail.com>
2022-08-11 10:36:19 +0900

    update

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,38 +1,16 @@
-struct Dove;
-struct Duck;
-
-trait Tweet {
-    fn tweet(&self);
-
-    fn tweet_twice(&self) {
-        self.tweet();
-        self.tweet();
-    }
-
-    fn shout(&self) {
-        println!("Uooooooohhh!!!!!");
-    }
-}
-
-impl Tweet for Dove {
-    fn tweet(&self) {
-        println!("Coo!");
-    }
-}
-
-impl Tweet for Duck {
-    fn tweet(&self) {
-        println!("Quack!");
-    }
+struct Color {
+    r: i32,
+    g: i32,
+    b: i32,
 }
 
 fn main() {
-    fn make_tuple<T, S>(t: T, s: S) -> (T, S) {
-        (t, s)
-    }
-
-    let t1 = make_tuple(1, 2);
-    let t2 = make_tuple("Hello", "world");
-    let t3 = make_tuple(vec![1, 2, 3], vec![4, 5]);
-    let t4 = make_tuple(3, "years old");
+    let a = Color {
+        r: 255,
+        g: 255,
+        b: 255,
+    };
+    let b = a; // 所有権が譲渡される
+    println!("{} {} {}", b.r, b.g, b.b);
+    println!("{:?}", a.r);
 }
