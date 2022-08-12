84d514947f1eed9d033801db550e2e9e0b327985
ktny <katsunori.t10@gmail.com>
2022-08-12 15:37:08 +0900

    add clap derive pattern

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "ktny",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    match opts.formula_file {
+        Some(file) => println!("File specified; {}", file),
+        None => println!("No file specified"),
+    }
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
