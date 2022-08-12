7283d3a19f5a55757a8c7c9d7491e488de11559a
ktny <katsunori.t10@gmail.com>
2022-08-12 17:46:16 +0900

    add error handling

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,5 +1,6 @@
 // 逆ポーランド記法での計算を行うCLIツール
 
+use anyhow::{bail, ensure, Context, Result};
 use clap::Parser;
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
@@ -22,18 +23,18 @@ struct Opts {
     formula_file: Option<String>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let opts = Opts::parse();
 
     if let Some(path) = opts.formula_file {
         // ファイル読み込み
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
-        run(reader, opts.verbose);
+        run(reader, opts.verbose)
     } else {
         let stdin = stdin();
         let reader = stdin.lock(); // ロックするとバッファリングして読み出せるようになり高速
-        run(reader, opts.verbose);
+        run(reader, opts.verbose)
     }
 }
 
@@ -43,14 +44,19 @@ fn main() {
  * @param reader: トレイトBufReadを実装している任意の型
  * @param verbose: 処理の詳細を出力するか
  */
-fn run<R: BufRead>(reader: R, verbose: bool) {
+fn run<R: BufRead>(reader: R, verbose: bool) -> Result<()> {
     let calc = RpnCalculator::new(verbose);
 
     for line in reader.lines() {
-        let line = line.unwrap();
-        let answer = calc.eval(&line);
-        println!("{}", answer);
+        let line = line?;
+
+        match calc.eval(&line) {
+            Ok(answer) => println!("{}", answer),
+            Err(e) => eprintln!("{:#?}", e),
+        }
     }
+
+    Ok(())
 }
 
 /**
@@ -71,7 +77,7 @@ impl RpnCalculator {
      * @param formula: 数式文字列
      * @return 計算結果
      */
-    pub fn eval(&self, formula: &str) -> i32 {
+    pub fn eval(&self, formula: &str) -> Result<i32> {
         // スペースで区切ってトークンリストにする
         let mut tokens = formula.split_whitespace().rev().collect();
         self.eval_inner(&mut tokens)
@@ -83,19 +89,22 @@ impl RpnCalculator {
      * @param toekns: 数式の要素となるトークンリスト
      * @return 計算結果
      */
-    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> Result<i32> {
         // トークンから読み出した要素をスタックするリスト
         let mut stack = Vec::new();
+        let mut pos = 0;
 
         // トークンの末尾から1つずつ読み出す
         while let Some(token) = tokens.pop() {
+            pos += 1;
+
             // 数値であればスタックに積む
             if let Ok(x) = token.parse::<i32>() {
                 stack.push(x);
             // 演算子であればスタックから2つ要素を取り出し計算して結果をスタックに積む
             } else {
-                let y = stack.pop().expect("invalid syntax");
-                let x = stack.pop().expect("invalid syntax");
+                let y = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let x = stack.pop().context(format!("invalid syntax at {}", pos))?;
 
                 let res = match token {
                     "+" => x + y,
@@ -103,7 +112,7 @@ impl RpnCalculator {
                     "*" => x * y,
                     "/" => x / y,
                     "%" => x % y,
-                    _ => panic!("invalid token"),
+                    _ => bail!("invalid token at {}", pos),
                 };
                 stack.push(res);
             }
@@ -114,11 +123,9 @@ impl RpnCalculator {
             }
         }
 
-        if stack.len() == 1 {
-            stack[0]
-        } else {
-            panic!("invalid syntax")
-        }
+        ensure!(stack.len() == 1, "invalid syntax");
+
+        Ok(stack[0])
     }
 }
 
@@ -130,20 +137,21 @@ mod tests {
     fn test_ok() {
         let calc = RpnCalculator::new(false);
 
-        assert_eq!(calc.eval("5"), 5);
-        assert_eq!(calc.eval("50"), 50);
-        assert_eq!(calc.eval("-50"), -50);
-        assert_eq!(calc.eval("2 3 +"), 5);
-        assert_eq!(calc.eval("2 3 *"), 6);
-        assert_eq!(calc.eval("2 3 -"), -1);
-        assert_eq!(calc.eval("2 3 /"), 0);
-        assert_eq!(calc.eval("2 3 %"), 2);
+        assert_eq!(calc.eval("5").unwrap(), 5);
+        assert_eq!(calc.eval("50").unwrap(), 50);
+        assert_eq!(calc.eval("-50").unwrap(), -50);
+        assert_eq!(calc.eval("2 3 +").unwrap(), 5);
+        assert_eq!(calc.eval("2 3 *").unwrap(), 6);
+        assert_eq!(calc.eval("2 3 -").unwrap(), -1);
+        assert_eq!(calc.eval("2 3 /").unwrap(), 0);
+        assert_eq!(calc.eval("2 3 %").unwrap(), 2);
     }
 
     #[test]
-    #[should_panic]
     fn test_ng() {
         let calc = RpnCalculator::new(false);
-        calc.eval("1 1 ^");
+        assert!(calc.eval("").is_err());
+        assert!(calc.eval("1 1 1 +").is_err());
+        assert!(calc.eval("+ 1 1").is_err());
     }
 }
