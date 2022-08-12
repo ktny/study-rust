61783b266049f9b50cff812613d23e2fae0a9ae0
ktny <katsunori.t10@gmail.com>
2022-08-12 16:15:59 +0900

    add eval

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -33,14 +33,81 @@ fn main() {
 }
 
 /**
- * バッファを1行ずつ読み込んで処理する
+ * バッファを1行ずつ読み込んで数式を計算する
  *
  * @param reader: トレイトBufReadを実装している任意の型
  * @param verbose: 処理の詳細を出力するか
  */
 fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
+    }
+}
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    /**
+     * 数式を計算する。内部処理はeval_innerで行う
+     *
+     * @param formula: 数式文字列
+     * @return 計算結果
+     */
+    pub fn eval(&self, formula: &str) -> i32 {
+        // スペースで区切ってトークンリストにする
+        let mut tokens = formula.split_whitespace().rev().collect();
+        self.eval_inner(&mut tokens)
+    }
+
+    /**
+     * 数式を計算する
+     *
+     * @param toekns: 数式の要素となるトークンリスト
+     * @return 計算結果
+     */
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        // トークンから読み出した要素をスタックするリスト
+        let mut stack = Vec::new();
+
+        // トークンの末尾から1つずつ読み出す
+        while let Some(token) = tokens.pop() {
+            // 数値であればスタックに積む
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            // 演算子であればスタックから2つ要素を取り出し計算して結果をスタックに積む
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+
+            // -vオプションが指定されている場合はこの時点のトークンとスタックの状態を出力
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
     }
 }
