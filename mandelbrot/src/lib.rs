db49a8711afec5e28c1579b1d3adf987e1083bfe
ktny <katsunori.t10@gmail.com>
2022-08-13 11:31:43 +0900

    add web-sys

--- mandelbrot/src/lib.rs
+++ mandelbrot/src/lib.rs
@@ -1,6 +1,9 @@
+mod logic;
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{Clamped, JsCast};
+use web_sys::ImageData;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +12,25 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(a: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, mandelbrot!");
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+macro_rules! measure_elapsed_time {
+    ($t:tt,$s:block) => {{
+        let window = web_sys::window().expect("should have a window in this context");
+        let performance = window
+            .performance()
+            .expect("performance should be available");
+        let start = performance.now();
+        let result = { $s };
+        let end = performance.now();
+        console_log!("{}:{}[ms]", $t, end - start);
+        result
+    }};
 }
